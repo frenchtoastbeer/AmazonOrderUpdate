2d6029eb217fe163681aa1e8fa4eb8e880094395
frenchtoastbeer <raymond@raymond.life>
2017-08-28 22:25:33 -0600

    Add files via upload

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports AmazonOrderUpdate
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("My sentance is: hAHAHAHA.")
+        Console.Write("The first character is ")
+        If Not AmazonOrderUpdate.UtilityLibraries.StartsWithUpper("hAHAHAHAHA") Then
+            Console.Write("not ")
+        End If
+        Console.Write("Capitalized")
+        Console.ReadLine()
+    End Sub
+
+End Module
